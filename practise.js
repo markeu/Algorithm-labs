7a677523343270c85b8c2c61281a9f9406c3dd26
markeu <ucheuzochukwumark@gmail.com>
2020-09-08 14:19:55 +0100

    tuesday sept 8

--- practise.js
+++ practise.js
@@ -295,3 +295,27 @@ const palindromeChainLength = (n) => {
 };
 
 console.log(palindromeChainLength(87));
+
+function matchMismatch(str) {
+    return str
+        .replace(/2/g, "Z")
+        .replace(/1/g, "I")
+        .replace(/8/g, "B")
+        .replace(/3/g, "E")
+        .replace(/0/g, "O")
+        .replace(/5/g, "S");
+}
+
+function similarLicensePlates(plate1, plate2) {
+    let plate1Arr = matchMismatch(plate1).trim().toLowerCase().split("");
+    let plate2Arr = matchMismatch(plate2).trim().toLowerCase().split("");
+    let result = {};
+    //console.log(plate1Arr, plate2Arr);
+    plate1Arr.forEach((key, i) => (result[key] = plate2Arr[i]));
+    //console.log(result);
+    for (const [key, value] of Object.entries(result)) {
+        return key == value ? true : false;
+    }
+}
+
+console.log(similarLicensePlates("AA", "A A A"));
