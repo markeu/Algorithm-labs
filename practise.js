b2fecda3bf7abdd3413a9c1e7b35b47664f530ba
markeu <ucheuzochukwumark@gmail.com>
2020-09-09 13:21:32 +0100

    wednesday 9th 2020

--- practise.js
+++ practise.js
@@ -19,6 +19,7 @@ function similarLicensePlates(plate1, plate2) {
         .trim()
         .toLowerCase()
         .split("");
+
     let result = {};
 
     plate1Arr.forEach((key, i) => (result[key] = plate2Arr[i]));
@@ -28,3 +29,27 @@ function similarLicensePlates(plate1, plate2) {
 }
 
 console.log(similarLicensePlates("AA", "A A A"));
+
+function calSumOfDigit(digits) {
+    const num = digits.toString().split("");
+    const realDigits = num.reduce(
+        (accumulator, currentValue) => +accumulator + +currentValue
+    );
+    return realDigits;
+}
+
+function orderWeight(strng) {
+    const seperatedNumberArr = strng.split(" ");
+    const result = seperatedNumberArr.sort((a, b) => {
+        let sumOfElem1 = calSumOfDigit(a);
+        let sumOfElem2 = calSumOfDigit(b);
+        return sumOfElem1 == sumOfElem2 ?
+            a.localeCompare(b) :
+            sumOfElem1 - sumOfElem2;
+    });
+
+    return result.join(" ");
+}
+
+console.log(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
+//"11 11 2000 10003 22 123 1234000 44444444 9999";
